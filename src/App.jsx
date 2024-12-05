5cd6b084c5c35ab142c9bcd4a420a6872b572cdd
syeda shakera <unisyeds808@gmail.com>
2024-12-05 10:15:07 -0700

    React Dom and components lesson2

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
-// create the 'todoList' array
-const todoList = [
-  {
-    id:1,
-    title:"React",
-  },
-  {
-    id:2,
-    title:"Learning",
-  },
-  {
-    id:3,
-    title: "Journey"
-  }
-];
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
       <h1>Todo List</h1>
-     {/* render the list using .map() */}
-      <ul>
-        {todoList.map((item) =>( 
-          <li key={item.id}> {item.title}</li>
-        ))}
-        
-      </ul>
+      <AddTodoForm />
+      {/* Use TodoList component */}
+      <TodoList />
     </div>
   );
 }
